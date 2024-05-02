862132abfd33adc3f4924c83316cffc5d1d01138
JiHwan Kim <98574943+Hizef@users.noreply.github.com>
2024-05-02 23:08:28 +0900

    [D1] Title: 아주 간단한 계산기, Time: 172 ms, Memory: 26,024 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum + secondNum);
+        System.out.println(firstNum - secondNum);
+        System.out.println(firstNum * secondNum);
+        System.out.println(firstNum / secondNum);
+    }
+}
